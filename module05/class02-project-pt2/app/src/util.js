27c9cbf3a5080ac7839dbee07b271eedafd33921
carloshkruger <carloshkruger@gmail.com>
2022-01-28 22:19:40 -0300

    working with regex project pt2

---
+++ module05/class02-project-pt2/app/src/util.js
@@ -0,0 +1,19 @@
+const safeRegex = require('safe-regex')
+
+class InvalidRegexError extends Error {
+  constructor(exp) {
+    super(`This ${exp} is unsafe dude`)
+    this.name = InvalidRegexError.name
+  }
+}
+
+const evaluateRegex = (exp) => {
+  const isSafe = safeRegex(exp)
+  if (isSafe) {
+    return exp
+  }
+
+  throw new InvalidRegexError(exp)
+}
+
+module.exports = { evaluateRegex, InvalidRegexError }
---
+++ module05/class02-project-pt2/app/test/textProcessorFluentAPI.test.js
@@ -0,0 +1,86 @@
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
+const TextProcessorFluentAPI = require('../src/textProcessorFluentAPI')
+const mock = require('./mock/valid')
+
+describe('TextProcessorFluentAPI', () => {
+  it('#build', () => {
+    const result = new TextProcessorFluentAPI(mock).build()
+    
+    expect(result).to.be.deep.equal(mock)
+  })
+
+  it('#extractPeopleData', () => {
+    const result = new TextProcessorFluentAPI(mock).extractPeopleData().build()
+    const expected = [
+      [
+        'Xuxa da Silva, brasileira, casada, CPF 235.743.420-12, residente e ',
+        'domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo. '
+      ].join('\n'),
+      [
+        'Arya Robbin, belga, casado, CPF 884.112.200-52, residente e ',
+        'domiciliada a Av. paulista, 1400, bairro Consolação, São Paulo. '
+      ].join('\n'),
+    ]
+
+    expect(result).to.be.deep.equal(expected)
+  })
+
+  it('divideTextInColumns', () => {
+    const content = [
+      [
+        'Xuxa da Silva, brasileira, casada, CPF 235.743.420-12, residente e ',
+        'domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo. '
+      ].join('\n'),
+    ]
+
+    const expected = [
+      [
+        'Xuxa da Silva',
+        ' brasileira',
+        ' casada',
+        ' CPF 235.743.420-12',
+        ' residente e \ndomiciliada a Rua dos bobos',
+        ' zero',
+        ' bairro Alphaville',
+        ' São Paulo. '
+      ]
+    ]
+
+    const result = new TextProcessorFluentAPI(content).divideTextInColumns().build()
+
+    expect(result).to.be.deep.equal(expected)
+  })
+
+  it('removeEmptyCharacters', () => {
+    const content = [
+      [
+        'Xuxa da Silva',
+        ' brasileira',
+        ' casada',
+        ' CPF 235.743.420-12',
+        ' residente e \ndomiciliada a Rua dos bobos',
+        ' zero',
+        ' bairro Alphaville',
+        ' São Paulo. '
+      ]
+    ]
+
+    const expected = [
+      [
+        'Xuxa da Silva',
+        'brasileira',
+        'casada',
+        'CPF 235.743.420-12',
+        'residente e domiciliada a Rua dos bobos',
+        'zero',
+        'bairro Alphaville',
+        'São Paulo.'
+      ]
+    ]
+
+    const result = new TextProcessorFluentAPI(content).removeEmptyCharacters().build()
+
+    expect(result).to.be.deep.equal(expected)
+  })
+})
